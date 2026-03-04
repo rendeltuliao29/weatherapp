487497b04db51ef798b825ad5dc13cf9260d408a
rendeltuliao29 <rendel.tuliao29@gmail.com>
2026-03-05 00:46:06 +0800

    Update API key

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -2,19 +2,21 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import '../models/weather_model.dart';
 
-
 class WeatherService {
-  final String apiKey = 'YOUR_API_KEY_HERE';
+  final String apiKey = '708043cbcd65e418d747c6a9ae0b4ae4';
   final String baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
-    Future<WeatherModel> fetchWeather(String city) async {
-          final url = Uri.parse(
+
+  Future<WeatherModel> fetchWeather(String city) async {
+    final url = Uri.parse(
       '$baseUrl?q=$city&appid=$apiKey&units=metric',
     );
-        final response = await http.get(url);
-            if (response.statusCode == 200) {
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
       final Map<String, dynamic> data = jsonDecode(response.body);
       return WeatherModel.fromJson(data);
-          } else {
+    } else {
       throw Exception('Failed: ${response.statusCode}');
     }
   }
